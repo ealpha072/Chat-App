9e666817371d58bb65b99dd2796c48ef63b7a035
ealpha072 <ealpha072@gmail.com>
2021-12-01 22:47:51 +0300

    Complete front-end

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
     <div class="card"  id="wrapper">
         <div class="card-header chat-user">
             <div class="arrow-back">
-                <a href=""><i class="fa fa-arrow-left"></i></a>
+                <a href="user.php"><i class="fa fa-arrow-left"></i></a>
             </div>
             <div class="img-name">
                 <img src="images/user.png" alt="">
@@ -18,16 +18,53 @@
         <div class="card-body chat-area">
             <div class="chat outgoing">
                 <div class="message">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam est accusamus quod, voluptatem, atque modi consequatur id perspiciatis veniam hic soluta? Amet pariatur sit, praesentium aliquid corporis eaque deserunt reprehenderit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam est accusamus quod, voluptatem, atque modi consequatur id perspiciatis veniam hic soluta? </p>
                 </div>
             </div>
+
             <div class="chat incoming">
                 <img src="images/user.png" alt="">
                 <div class="message">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, nihil molestiae repudiandae a inventore maxime, soluta reprehenderit optio, quam porro beatae? Porro perspiciatis aliquam earum unde cum modi hic voluptas. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, nihil molestiae repudiandae a inventore maxime, soluta reprehenderit optio </p>
                 </div>
             </div>
+
+            <div class="chat outgoing">
+                <div class="message">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam est accusamus quod, voluptatem, atque modi consequatur id perspiciatis veniam hic soluta? </p>
+                </div>
+            </div>
+            
+            <div class="chat incoming">
+                <img src="images/user.png" alt="">
+                <div class="message">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, nihil molestiae repudiandae a inventore maxime, soluta reprehenderit optio </p>
+                </div>
+            </div>
+
+            <div class="chat incoming">
+                <img src="images/user.png" alt="">
+                <div class="message">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, nihil molestiae repudiandae a inventore maxime, soluta reprehenderit optio </p>
+                </div>
+            </div>
+
+            <div class="chat outgoing">
+                <div class="message">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam est accusamus quod, voluptatem, atque modi consequatur id perspiciatis veniam hic soluta? </p>
+                </div>
+            </div> 
         </div>
+
+        <div class="typing-area">
+                <form action="" class="">
+                    <div class="form-group">
+                        <input type="text" placeholder="Type message" class="form-control shadow-none">
+                        <button class="btn btn-dark btn-sm shadow-none"><i class="fa fa-paper-plane"></i> </button>
+                    </div>
+                </form>
+        </div>
+
     </div>
 </div>
 
