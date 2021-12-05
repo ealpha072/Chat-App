70df59b8828afb94ba18da090d1e24856d6f659e
ealpha072 <ealpha072@gmail.com>
2021-12-05 23:46:48 +0300

    Get latest message

--- php/classes/access.php
+++ php/classes/access.php
@@ -234,6 +234,10 @@
                 return $stmt;
             }
 
+            public function getLastMessage(){
+                $sql = 'SELECT * FROM '.$this->tablename.' WHERE (incoming_msg_id=:incoming_id OR outgoing_msg_id=:outgoing_id) AND (incoming_msg_id=:outgoing_id AND outgoing_msg_id=:incoming_id) ORDER BY msg_id LIMIT 1';
+            }
+
     }
 
 ?>
